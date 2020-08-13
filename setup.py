37adf5cccfbe1f491c68ed703eb9e8843577ff0e
Brodie Norfolk <brodiejamesnorfolk@gmail.com>
2020-08-13 20:54:34 +1000

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydisk',
+      description='Python astronomy tools',
+      url='http://github.com/bjnorfolk/pymcfost',
+      author='Brodie Norfolk',
+      license='MIT',
+      packages=['pydisk'],
+      zip_safe=False)
