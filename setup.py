27e4320d446317c1dcd3b6eccd4f51776718bb12
Brodie Norfolk <brodiejamesnorfolk@gmail.com>
2020-08-24 15:53:47 +1000

    added package check

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pydisk',
       description='Python astronomy tools',
       url='http://github.com/bjnorfolk/pymcfost',
+      packages=find_packages(),
+      install_requires=[line.rstrip() for line in open("requirements.txt", "r").readlines()],
       author='Brodie Norfolk',
       license='MIT',
       packages=['pydisk'],
