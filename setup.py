235276cf46e70e80881ed9d34894658ed54652e2
Brodie Norfolk <brodiejamesnorfolk@gmail.com>
2020-08-24 15:57:38 +1000

    typos

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='pydisk',
       description='Python astronomy tools',
-      url='http://github.com/bjnorfolk/pymcfost',
+      version=0.0.0,
+      url='http://github.com/bjnorfolk/pydisk',
+      python_requires='>=3',
       packages=find_packages(),
       install_requires=[line.rstrip() for line in open("requirements.txt", "r").readlines()],
       author='Brodie Norfolk',
       license='MIT',
-      packages=['pydisk'],
       zip_safe=False)
