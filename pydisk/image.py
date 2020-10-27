427ddece30914a79af9f07cd8098a8fc728d64cd
Brodie Norfolk <brodiejamesnorfolk@gmail.com>
2020-10-27 17:40:45 +1100

    image works! ... for the most part

--- pydisk/image.py
+++ pydisk/image.py
@@ -8,8 +8,16 @@ from numpy import ndarray
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.ticker import FormatStrFormatter
 
-from .utils import readfits, getdeg
+from pathlib import Path
+
+from astropy.wcs import WCS
+
+from copy import copy
+
+from .utils import readfits, getdeg, DEGTORAD
 
 class image:
 	"""Image FITS object.
@@ -95,7 +103,7 @@ class image:
 		y=np.arange(0,ysize,1)
 		import warnings
 		warnings.filterwarnings("ignore")
-		w=WCS(filename)
+		w=WCS(self.filename)
 		warnings.resetwarnings()
 
 		try: #should work in most cases
@@ -105,7 +113,7 @@ class image:
 				lon,lat=w.wcs_pix2world(x,y,1)
 			if(lon[1]<0.):
 				lon=np.array(lon)+360.
-			xr=(lon-targetra_ang)*3600.*np.cos(targetdec_ang*DEGTORAD)
+			xr=(lon-targetra_ang)*3600.*np.cos(DEGTORAD(targetdec_ang))
 			yr=(lat-targetdec_ang)*3600.
 		except TypeError:
 			xpscale=he['CDELT1']*3600.
@@ -117,14 +125,28 @@ class image:
 			xr=np.arange(-1*xpscale*cxpx,xpscale*(xsize-cxpx),xpscale)+(cxval-targetra_ang)*3600.
 			yr=np.arange(-1*ypscale*cypx,ypscale*(ysize-cypx),ypscale)+(cyval-targetdec_ang)*3600.
 
-		if (xr.shape!=im.shape[1]):
-			xr = xr[0:im.shape[1]]
+		if (xr.shape[0]!=contmap.shape[1]):
+			xr = xr[0:contmap.shape[1]]
 			print('xr array corrected')
-		if (yr.shape!=im.shape[0]):
-			yr = yr[0:im.shape[0]]
+		if (yr.shape[0]!=contmap.shape[0]):
+			yr = yr[0:contmap.shape[0]]
 			print('yr array corrected')
 
-		return contmap, x, y
+		x, y = np.meshgrid(xr,yr)
+
+		rr = np.sqrt(x**2+y**2)
+
+		#Reshaping if x!=y
+		if (rr.shape!=contmap.shape):
+			rr = rr[0:contmap.shape[0],0:contmap.shape[1]]
+
+		#Greater than any typical PPD radius
+		radius = 3
+
+		w = np.where(rr>radius)
+		rms = np.std(contmap[w])
+
+		return contmap, x, y, rms
 
 	def plot(self,
 		plot_star: bool = False,
@@ -196,34 +218,41 @@ class image:
 		else:
 			fig = ax.figure
 
-		# self._read[self]
-
 		#produce contour map from fits file
-		contmap, x, y = self.contour_map() #test
+		contmap, x, y, rms = self.contour_map()
+		he = self.he
+		_kwargs = copy(map_kwargs)
+		cmap = _kwargs.pop('cmap', 'inferno')
 
-		ax.contourf(x, y, contmap, rasterized=True, **map_kwargs)
+		contf = ax.contourf(x, y, contmap*10**3, levels=1000, cmap=cmap, zorder= 1, rasterized=True, **map_kwargs)
 
 		if contours:
 			_kwargs = copy(contour_kwargs)
-			alpha = _kwargs.pop('alpha', '0.7')
+			alpha = _kwargs.pop('alpha', 0.7)
 			color = _kwargs.pop('color', 'w')
-			lw = _kwargs.pop('linewidth', '2')
-			ax.contour(x,y,im,contours, alpha=alpha, linewidths=lw, 
+			lw = _kwargs.pop('linewidth', 1)
+
+			contour_array = np.array(contours)
+			contours_neg = -1*contour_array
+			levels = np.sort(np.append(contours_neg,contour_array))*rms
+			ax.contour(x,y,contmap,levels, alpha=alpha, linewidths=lw, 
 				colors=color, zorder=2, rasterized=True)
 
 		#colorbar
 		if show_colorbar:
-			divider = make_axes_locatable(ax)
 			_kwargs = copy(colorbar_kwargs)
+			label = _kwargs.pop('label', 'mJy/beam')
 			position = _kwargs.pop('position', 'right')
 			size = _kwargs.pop('size', '5%')
 			pad = _kwargs.pop('pad', '2%')
 			if position in ('top', 'bottom'):
 				_kwargs.update({'orientation': 'horizontal'})
-			cax = divider.append_axes(position=position, size=size, pad=pad)
-			cbar = fig.colorbar(im*10**3, cax, **_kwargs)
 
-			label = _kwargs('label', 'mJy/beam')
+			divider = make_axes_locatable(ax)
+			cax = divider.append_axes(position=position, size=size, pad=pad)
+			cbar = plt.colorbar(contf, cax, **_kwargs)
+			#cbar.ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
 			cbar.set_label(label)
 
 		if plot_star:
@@ -248,10 +277,10 @@ class image:
 
 			ell=Ellipse(xy=(0.7*sz,-0.7*sz), width=bmj*3600.,
 				height=bmn*3600., angle=bmpa, edgecolor=clr, 
-				hatch='\\\\\\', fc='None',linewidth=lw)
+				hatch='\\\\\\', fc='None',linewidth=lw, zorder=5)
 			ell1=Ellipse(xy=(0.7*sz,-0.7*sz), width=bmj*3600.,
 				height=bmn*3600., angle=bmpa, edgecolor=clr, 
-				hatch='////', fc='None',linewidth=lw)
+				hatch='////', fc='None',linewidth=lw, zorder=5)
 			ax.add_patch(ell)
 			ax.add_patch(ell1)
 
